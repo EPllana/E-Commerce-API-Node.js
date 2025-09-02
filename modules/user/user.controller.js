f8894b2cd3afd4e86ac145daeb68e5269854648a
Ernis Pllana <epllana03@gmail.com>
2025-09-03 01:41:44 +0200

    Created User.Model.js

---
+++ modules/user/user.controller.js
@@ -0,0 +1,7 @@
+export const createUser = async(req,res)=>{
+    try{
+
+    }catch(error){
+        
+    }
+}
---
+++ modules/user/user.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+    type:String,
+    required:true
+    },
+    surname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        requried:true,
+        unique:true
+    },
+    phoneNumber:{
+        type:String,
+        default:null
+    },
+    password:{
+        type:String,
+        required:true,
+        minlength:8
+    },
+    isActive:{
+        type:Boolean,
+        default:"true"
+    },
+    isVerified:{
+        type:Boolean,
+        default:"false"
+    },
+
+    roole:{
+        type:String,
+        enum:["admin","user","moderator"],
+        default:"user"
+    },
+},
+{timestampts:true});
+
+const User = mongoose.model("user", userSchema);
+
+export default User;
